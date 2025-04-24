f86d2cc0b00090233cff32d8c7d1f9c9acf1206a
xyryc <amdanikzz@gmail.com>
2025-04-24 13:47:00 +0600

    fixed pre-rendering issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import ProductCard from "@/components/ProductCard";
 import { fetchAllProducts } from "@/lib/api";
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   const products = await fetchAllProducts()
   // console.log(products)
