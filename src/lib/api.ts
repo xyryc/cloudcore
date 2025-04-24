081f5afab8aa445f6480b3e67f9fc158c4841b64
xyryc <amdanikzz@gmail.com>
2025-04-24 18:23:07 +0600

    removed no-cache

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,8 +3,7 @@ import { Product } from "@/types";
 export async function fetchAllProducts(): Promise<Product[]> {
   try {
     const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_BASE}/api/all/product/get`,
-      { cache: "no-store" }
+      `${process.env.NEXT_PUBLIC_API_BASE}/api/all/product/get`
     );
 
     if (!res.ok) {
