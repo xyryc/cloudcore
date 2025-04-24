531fe80af4c0189e41246ef3efcb8d67bab37e49
xyryc <amdanikzz@gmail.com>
2025-04-24 14:27:07 +0600

    updated

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,9 +1,9 @@
 import { Product } from "@/types";
 
-const API_BASE = "https://admin.refabry.com/api";
-
 export async function fetchAllProducts(): Promise<Product[]> {
-  const res = await fetch(`${API_BASE}/all/product/get`);
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE}/all/product/get`
+  );
   const { data } = await res.json();
   return data.data;
 }
