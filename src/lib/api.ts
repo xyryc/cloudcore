5959f2902ac3f104f850b7e664e9a9d44d19f858
xyryc <amdanikzz@gmail.com>
2025-04-24 14:30:14 +0600

    updated api url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import { Product } from "@/types";
 
 export async function fetchAllProducts(): Promise<Product[]> {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE}/all/product/get`
+    `${process.env.NEXT_PUBLIC_API_BASE}/api/all/product/get`
   );
   const { data } = await res.json();
   return data.data;
